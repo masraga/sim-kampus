73af06fe98deb14dd13cacc99874a13833af8e3c
masraga <real.ragamulia@gmail.com>
2021-09-01 15:26:03 +0700

    membuat sistem layouting

---
+++ app/Views/bendahara/templates/sidebar.php
@@ -0,0 +1,30 @@
+<div class="main-sidebar">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+          </div>
+          <ul class="sidebar-menu">
+              <li class="menu-header">Dashboard</li>
+              <li class="nav-item dropdown active">
+                <a href="<?php echo base_url() ?>/bendahara" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <!-- <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a> -->
+                <!-- <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                  <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                </ul> -->
+              </li>
+              <li class="menu-header">Tagihan</li>
+          
+              <li><a class="nav-link" href="/bendahara/tagihan/new"><i class="far fa-square"></i> <span>Tambah Tagihan</span></a></li>
+            </ul>
+
+            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+              <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                <i class="fas fa-rocket"></i> Documentation
+              </a>
+            </div>
+        </aside>
+      </div>
