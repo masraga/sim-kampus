e516f342652f8bf64a1d30ac9eea20e8e97509c1
masraga <real.ragamulia@gmail.com>
2021-09-01 17:14:10 +0700

    membuat controller untuk menambahkan tagihan baru

---
+++ app/Controllers/Tagihan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Services\Tagihan\TagihanManager;
+
+class Tagihan extends BaseController
+{
+	public function add()
+	{
+		$tagihan = TagihanManager::add( $this->request->getPost() );
+
+		session()->setFlashdata( "alert-msg", $tagihan["msg"] );
+		session()->setFlashdata( "alert-code", $tagihan["code"] );
+		
+		if( $tagihan["code"] == 200  ) {
+			$tokenMahasiswa = $tagihan["data"]["nim"];
+			return redirect()->to( site_url( "/bendahara/tagihan/mahasiswa?token={$tokenMahasiswa}" ) );
+		}
+
+		return redirect()->to( site_url( "/bendahara/tagihan/new" ) );
+	}
+}
