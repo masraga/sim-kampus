8e95ebbf78dae6bcdb320c9a57c80e001e0fa1b2
masraga <real.ragamulia@gmail.com>
2021-09-01 23:20:54 +0700

    menampilkan data tagihan mahasiswa

--- app/Views/mahasiswa/home.php
+++ app/Views/mahasiswa/home.php
@@ -8,10 +8,41 @@
     </div>
 
     <div class="section-body">
-      
-      <!-- tambahkan data disini -->
+      <div class="card">
+        <div class="card-header">
+          <h4>Data tagihan kamu</h4>
+        </div>
+        <div class="card-body">
+          <h4>Tagihan Lunas</h4>
+          <table class="table" border="1" id="table-lunas">
+            <thead>
+              <tr>
+                <th>Semester</th>
+                <th>Jenis Tagihan</th>
+                <th>Jumlah Tagihan</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
 
+          <br>
+          <br>
 
+          <h4>Tagihan Belum Lunas</h4>
+          <table class="table" border="1" id="table-unlunas">
+            <thead>
+              <tr>
+                <th>Semester</th>
+                <th>Jenis Tagihan</th>
+                <th>Jumlah Tagihan</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+
+        </div>
+      </div>
     </div>
   </section>
 </div>
@@ -21,7 +52,41 @@
 <?php $this->section( "custom-js" ) ?>
 
 <script type="text/javascript">
+  
+  // mengambil list data tagihan mahasiswa
+  function getTagihan()
+  {
+    fetch( `${BASE_URL}/api/mahasiswa/tagihan?use-email=true` )
+    .then( response => response.json() )
+    .then( response => {
+      for( let data of response.data ) {
+        const isLunas = Boolean( Number( data.is_lunas ) );
+        if( isLunas ) {
+          $( `#table-lunas > tbody` ).append(`
+            <tr>
+              <td>${data.mahasiswa_semester}</td>
+              <td>${data.jenis_tagihan}</td>
+              <td>${data.jumlah_tagihan}</td>
+            </tr>
+          `);
+        }
+        else {
+          $( `#table-unlunas > tbody` ).append(`
+            <tr>
+              <td>${data.mahasiswa_semester}</td>
+              <td>${data.jenis_tagihan}</td>
+              <td>${data.jumlah_tagihan}</td>
+              <td><a href=>hapus</a></td>
+            </tr>
+          `);
+        }
+      }
+    } )
+  }
 
+  $(document).ready( function(e) {
+    getTagihan();
+  } )
 </script>
 
 <?php $this->endSection() ?>
